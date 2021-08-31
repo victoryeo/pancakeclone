ad23f1f1350751275eeaef5c543c26730614a2db
yeosiowvic <victoryeo1974@gmail.com>
2021-08-31 16:33:09 +0800

    add web3 and get the first account address

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -8,8 +8,38 @@ import { Settings } from './pages/Settings';
 import { Trade } from './pages/Trade';
 import { Pool } from './pages/Pool';
 import SideBar from './components/SideBar';
+import getWeb3 from './web3/getWeb3'
 
-function App() {
+const App = () => {
+  const [myWeb3, setMyWeb3] = useState();
+
+  useEffect(() => {
+    console.log('component mounted');
+    async function getW3() {
+      try {
+        let web3 = await getWeb3
+        console.log(web3)
+        setMyWeb3(web3)
+      } catch( err ) {
+        console.warn('Error in web3 initialization.', err)
+      }
+    }
+    getW3()
+    return () => console.log('component is being removed');
+  }, []);
+
+  useEffect(() => {
+    console.log(myWeb3)
+    async function getAcct() {
+      console.log(myWeb3.eth)
+      let accounts = await myWeb3.eth.getAccounts()
+      console.log(accounts[0])
+    }
+    if (myWeb3 != undefined) {
+      getAcct()
+    }
+  }, [myWeb3])
+
   return (
     <React.Fragment>
       <Router>
