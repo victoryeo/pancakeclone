44f89b38e124de5c0cf166c66187af08332a4835
yeosiowvic <victoryeo1974@gmail.com>
2021-09-02 14:18:35 +0800

    add acct to redux state

--- src/frontend/src/actions/actions.js
+++ src/frontend/src/actions/actions.js
@@ -2,6 +2,8 @@ import { createAction } from '@reduxjs/toolkit'
 
 export const actionTypes = {
   UPDATE_DUMMY : 'UPDATE_DUMMY',
+  UPDATE_ACCT  : 'UPDATE_ACCT',
 }
 
 export const STPupdateDummy = createAction(actionTypes.UPDATE_DUMMY)
+export const STPupdateAcct = createAction(actionTypes.UPDATE_ACCT)
--- src/frontend/src/reducers/reducers.js
+++ src/frontend/src/reducers/reducers.js
@@ -1,19 +1,29 @@
 import { createReducer } from '@reduxjs/toolkit'
-import { STPupdateDummy } from '../actions/actions'
+import { STPupdateDummy, STPupdateAcct } from '../actions/actions'
 
 export const initialState = {
   dummy: 'none',
+  acct:  0x0,
 }
 
 export const reducers = createReducer(initialState, (builder) => {
-  builder.addCase(STPupdateDummy, (state , action) => {
-    console.log(state.dummy)
-    console.log(action)
-    return {
-      ...state,
-      dummy: action.payload,
-    };
-  })
+  builder
+    .addCase(STPupdateDummy, (state , action) => {
+      console.log(state.dummy)
+      console.log(action)
+      return {
+        ...state,
+        dummy: action.payload,
+      };
+    })
+    .addCase(STPupdateAcct, (state , action) => {
+      console.log(state.acct)
+      console.log(action)
+      return {
+        ...state,
+        acct: action.payload,
+      };
+    })    
 })
 
 export default reducers;
