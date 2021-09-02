a7a171ca0110e8ac3471aff440ead44a35923bfc
yeosiowvic <victoryeo1974@gmail.com>
2021-09-02 12:28:11 +0800

    use configureStore instead of createStore configureStore is a wrapper over createStore

--- src/frontend/src/store/store.js
+++ src/frontend/src/store/store.js
@@ -1,26 +1,8 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk';
-
-import { persistStore, persistReducer } from 'redux-persist';
+import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from '../reducers/index.js';
-import storage from 'redux-persist/lib/storage';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-}
+export const store = configureStore({
+  reducer: rootReducer
+})
 
-const middlewares = [thunk];
-const isChrome = window.navigator.userAgent.includes('Chrome');
 
-export const store = createStore(
-  persistReducer(persistConfig, rootReducer),
-  compose(
-    applyMiddleware(...middlewares),
-
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-	  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ :
-       compose
-
-  )
-)
