415bc01468b8e77136eec238ab6fce2189c22f0f
yeosiowvic <victoryeo1974@gmail.com>
2021-09-02 13:59:00 +0800

    add redux-logger middleware

--- src/frontend/src/store/store.js
+++ src/frontend/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from '../reducers/index.js';
+import logger from 'redux-logger'
 
 export const store = configureStore({
-  reducer: rootReducer
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 })
 
 
