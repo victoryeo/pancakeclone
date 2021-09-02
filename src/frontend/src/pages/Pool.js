c1c9d35c4a423ef54b2f3c7e9c7fd305ab23d14c
yeosiowvic <victoryeo1974@gmail.com>
2021-09-02 12:30:00 +0800

    add useDispatch and useStore, call dispatch

--- src/frontend/src/pages/Pool.js
+++ src/frontend/src/pages/Pool.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useDispatch, useStore } from 'react-redux'
+import {
+  STPupdateDummy,
+ } from '../actions/actions.js'
+
 const Wrapper = styled.div`
   margin-top: 1em;
   margin-left: 6em;
   margin-right: 6em;
 `;
-export const Pool = () => (
-  <Wrapper>
-    <h2>Pool</h2>
-  </Wrapper>
-)
+
+function select(state) { 
+  console.log(state.reducers) 
+  return state.reducers.dummy
+}
+
+export const Pool = () => {
+  const dispatch = useDispatch()
+  const store = useStore()
+
+  return (<Wrapper>
+    <span>{select(store.getState())}</span>
+    <button onClick={() => dispatch( STPupdateDummy("test") )}>
+      Update Pool
+    </button>    
+    </Wrapper>)
+
+}
