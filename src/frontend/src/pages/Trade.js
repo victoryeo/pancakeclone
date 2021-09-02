dbdd5bca4fca8fc456576ccc2c1d7e7458e39578
yeosiowvic <victoryeo1974@gmail.com>
2021-09-02 15:02:21 +0800

    add useSelector to show acct address

--- src/frontend/src/pages/Trade.js
+++ src/frontend/src/pages/Trade.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useSelector } from 'react-redux'
+
 const Wrapper = styled.div`
   margin-top: 1em;
   margin-left: 6em;
   margin-right: 6em;
 `;
-export const Trade = () => (
-  <Wrapper>
-    <h2>Trade</h2>
-  </Wrapper>
-)
+
+export const Trade = () => {
+  const acct = useSelector((state) => state.reducers.acct)
+  console.log(acct)
+  return (
+    <Wrapper>
+      <h2>Trade</h2>
+      <div>{acct}</div>
+    </Wrapper>
+  )
+}
