2340f7de59fae58e89a07ee1acb00339604f3e4d
yeosiowvic <victoryeo1974@gmail.com>
2021-09-08 11:55:29 +0800

    update deploy script to deploy wbnb, cake, staking contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,15 +1,29 @@
 const { ethers } = require('hardhat');
 
 async function main() {
-  const [deployer] = await ethers.getSigners();
+  const [deployer, minter] = await ethers.getSigners();
 
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
+  console.log("Minter account:", minter.address);
 
-  // deploy library
+  // deploy wbnb
+  const WBNB = await ethers.getContractFactory("WBNB");
+  console.log('Deploying ...');
+  const wbnb = await WBNB.deploy();
+  console.log("the contract address:", wbnb.address);
+  
+  // deploy cake
+  const CakeToken = await ethers.getContractFactory("CakeToken");
+  console.log('Deploying ...');
+  const caketoken = await CakeToken.deploy();
+  console.log("the contract address:", caketoken.address);
+
+  // deploy staking
   const Con01 = await ethers.getContractFactory("BnbStaking");
   console.log('Deploying ...');
-  const con01 = await Con01.deploy();
+  const con01 = await Con01.deploy(wbnb.address, caketoken.address,
+    1000,10,1010, deployer.address, wbnb.address);
   console.log("the contract address:", con01.address);
 }
 
