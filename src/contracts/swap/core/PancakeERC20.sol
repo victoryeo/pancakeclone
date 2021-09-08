6eac5a7b4813da365d243510ac9ff4a72a2c3e9e
yeosiowvic <victoryeo1974@gmail.com>
2021-09-08 13:22:23 +0800

    fix solidity compilation errors

--- src/contracts/swap/core/PancakeERC20.sol
+++ src/contracts/swap/core/PancakeERC20.sol
@@ -8,30 +8,30 @@ import './libraries/SafeMath.sol';
 contract PancakeERC20 is IPancakeERC20 {
     using SafeMath for uint;
 
-    string public constant name = 'Pancake LPs';
-    string public constant symbol = 'Cake-LP';
-    uint8 public constant decimals = 18;
-    uint  public totalSupply;
-    mapping(address => uint) public balanceOf;
-    mapping(address => mapping(address => uint)) public allowance;
+    string public constant _name = 'Pancake LPs';
+    string public constant _symbol = 'Cake-LP';
+    uint8 public constant _decimals = 18;
+    uint  public _totalSupply;
+    mapping(address => uint) public _balanceOf;
+    mapping(address => mapping(address => uint)) public _allowance;
 
-    bytes32 public DOMAIN_SEPARATOR;
+    bytes32 public _DOMAIN_SEPARATOR;
     // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
-    bytes32 public constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
-    mapping(address => uint) public nonces;
+    bytes32 public constant _PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
+    mapping(address => uint) public _nonces;
 
-    event Approval(address indexed owner, address indexed spender, uint value);
-    event Transfer(address indexed from, address indexed to, uint value);
+    //event Approval(address indexed owner, address indexed spender, uint value);
+    //event Transfer(address indexed from, address indexed to, uint value);
 
-    constructor() public {
+    constructor() {
         uint chainId;
         assembly {
-            chainId := chainid
+            chainId := chainid()
         }
-        DOMAIN_SEPARATOR = keccak256(
+        _DOMAIN_SEPARATOR = keccak256(
             abi.encode(
                 keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'),
-                keccak256(bytes(name)),
+                keccak256(bytes(_name)),
                 keccak256(bytes('1')),
                 chainId,
                 address(this)
@@ -39,54 +39,90 @@ contract PancakeERC20 is IPancakeERC20 {
         );
     }
 
+    function name() public override pure returns (string memory) {
+        return _name;
+    }
+
+    function decimals() public override pure returns (uint8) {
+        return _decimals;
+    }
+
+    function symbol() public override pure returns (string memory) {
+        return _symbol;
+    }
+
+    function totalSupply() public override view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function allowance(address owner, address spender) public override view returns (uint256) {
+        return _allowance[owner][spender];
+    }
+
+    function balanceOf(address account) public override view returns (uint256) {
+        return _balanceOf[account];
+    }
+
+    function nonces(address account) public override view returns (uint256) {
+        return _nonces[account];
+    }
+
+    function DOMAIN_SEPARATOR() external override view returns (bytes32) {
+      return _DOMAIN_SEPARATOR;
+    }
+
+    function PERMIT_TYPEHASH() external override pure returns (bytes32) {
+        return _PERMIT_TYPEHASH;
+    }
+
     function _mint(address to, uint value) internal {
-        totalSupply = totalSupply.add(value);
-        balanceOf[to] = balanceOf[to].add(value);
+        _totalSupply = _totalSupply.add(value);
+        _balanceOf[to] = _balanceOf[to].add(value);
         emit Transfer(address(0), to, value);
     }
 
     function _burn(address from, uint value) internal {
-        balanceOf[from] = balanceOf[from].sub(value);
-        totalSupply = totalSupply.sub(value);
+        _balanceOf[from] = _balanceOf[from].sub(value);
+        _totalSupply = _totalSupply.sub(value);
         emit Transfer(from, address(0), value);
     }
 
     function _approve(address owner, address spender, uint value) private {
-        allowance[owner][spender] = value;
+        _allowance[owner][spender] = value;
         emit Approval(owner, spender, value);
     }
 
     function _transfer(address from, address to, uint value) private {
-        balanceOf[from] = balanceOf[from].sub(value);
-        balanceOf[to] = balanceOf[to].add(value);
+        _balanceOf[from] = _balanceOf[from].sub(value);
+        _balanceOf[to] = _balanceOf[to].add(value);
         emit Transfer(from, to, value);
     }
 
-    function approve(address spender, uint value) external returns (bool) {
+    function approve(address spender, uint value) external override returns (bool) {
         _approve(msg.sender, spender, value);
         return true;
     }
 
-    function transfer(address to, uint value) external returns (bool) {
+    function transfer(address to, uint value) external override returns (bool) {
         _transfer(msg.sender, to, value);
         return true;
     }
 
-    function transferFrom(address from, address to, uint value) external returns (bool) {
-        if (allowance[from][msg.sender] != uint(-1)) {
-            allowance[from][msg.sender] = allowance[from][msg.sender].sub(value);
+    function transferFrom(address from, address to, uint value) external override returns (bool) {
+        if (_allowance[from][msg.sender] != type(uint).max) {
+            _allowance[from][msg.sender] = _allowance[from][msg.sender].sub(value);
         }
         _transfer(from, to, value);
         return true;
     }
 
-    function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external {
+    function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external override {
         require(deadline >= block.timestamp, 'Pancake: EXPIRED');
         bytes32 digest = keccak256(
             abi.encodePacked(
                 '\x19\x01',
-                DOMAIN_SEPARATOR,
-                keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonces[owner]++, deadline))
+                _DOMAIN_SEPARATOR,
+                keccak256(abi.encode(_PERMIT_TYPEHASH, owner, spender, value, _nonces[owner]++, deadline))
             )
         );
         address recoveredAddress = ecrecover(digest, v, r, s);
