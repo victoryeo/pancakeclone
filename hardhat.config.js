85d4332478ea6a4ad56c29c0f8287519a57145d9
yeosiowvic <victoryeo1974@gmail.com>
2021-09-11 14:12:55 +0800

    add bsc testnet settings for hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
 
+const { mnemonic } = require('./secrets.json');
+
+// see https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
@@ -11,6 +22,12 @@ module.exports = {
   networks: {
     development: {
       url: "http://127.0.0.1:9545"
+    },
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: {mnemonic: mnemonic}
     },
   },
   paths: {
