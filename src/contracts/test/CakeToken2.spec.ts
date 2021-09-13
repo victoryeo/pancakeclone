ce096a5dcea4b7273fe90595f05a64202a42fafd
yeosiowvic <victoryeo1974@gmail.com>
2021-09-13 22:15:48 +0800

    streamline the CakeToken test case

--- src/contracts/test/CakeToken2.spec.ts
+++ src/contracts/test/CakeToken2.spec.ts
@@ -1,25 +1,25 @@
 import { ethers } from "hardhat"
 import { assert } from "chai"
+import { Contract } from "ethers"
 
 describe("CakeToken", function () {
-  it("mint", async function () {
+  let ct: Contract
+  beforeEach(async function () {
     const CakeToken = await ethers.getContractFactory("CakeToken")
     // CakeToken is a factory for instances of CakeToken contract.
-    const ct = await CakeToken.deploy()
+    ct = await CakeToken.deploy()
     await ct.deployed()
-
+  })
+  
+  it("mint", async function () {
     const [alice] = await ethers.getSigners()
     await ct['mint(address,uint256)'](alice.address, 1000)
     assert.equal((await ct.balanceOf(alice.address)).toString(), '1000')
   })
 
   it("vote", async function () {
-    const CakeToken = await ethers.getContractFactory("CakeToken")
-    // CakeToken is a factory for instances of CakeToken contract.
-    const ct = await CakeToken.deploy()
-    await ct.deployed()
-
     const [alice, addr1] = await ethers.getSigners()
+    //execute a contract's method from another account, use connect
     let vote = await ct.connect(addr1).getCurrentVotes(alice.address)
     console.log(vote.toString())
     assert.equal(vote, 0)
