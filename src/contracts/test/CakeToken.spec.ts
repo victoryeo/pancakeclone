fbeaa46202b047cca2000e607a7855dd49363f21
yeosiowvic <victoryeo1974@gmail.com>
2021-09-14 07:59:37 +0800

    add comments to differentiate two different ways of testing CakeToken

--- src/contracts/test/CakeToken.spec.ts
+++ src/contracts/test/CakeToken.spec.ts
@@ -7,6 +7,7 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
 import CakeToken from '../../../artifacts/src/contracts/pool/CakeToken.sol/CakeToken.json'
 
 describe("CakeToken", function () {
+  // using mock provider to get wallet address
   const provider = new MockProvider()
   const [wallet, alice] = provider.getWallets()
   let token: Contract
@@ -15,11 +16,8 @@ describe("CakeToken", function () {
 
   beforeEach(async function () {
     [bob, minter] = await ethers.getSigners()
+    // use wallet to send the deploy transaction
     token = await deployContract(wallet, CakeToken, [])
-  })
-
-  it('test', async () => {
-    //to be added
   })
 
   it('mint', async () => {
--- src/contracts/test/CakeToken2.spec.ts
+++ src/contracts/test/CakeToken2.spec.ts
@@ -5,13 +5,15 @@ import { Contract } from "ethers"
 describe("CakeToken", function () {
   let ct: Contract
   beforeEach(async function () {
+    // CakeToken is a factory for instances of cakeToken contract
     const CakeToken = await ethers.getContractFactory("CakeToken")
-    // CakeToken is a factory for instances of CakeToken contract.
     ct = await CakeToken.deploy()
     await ct.deployed()
   })
   
   it("mint", async function () {
+    // Signer in Ethers.js is an object that represents an Ethereum account
+    // user address from Signer to test
     const [alice] = await ethers.getSigners()
     await ct['mint(address,uint256)'](alice.address, 1000)
     assert.equal((await ct.balanceOf(alice.address)).toString(), '1000')
@@ -19,7 +21,7 @@ describe("CakeToken", function () {
 
   it("vote", async function () {
     const [alice, addr1] = await ethers.getSigners()
-    //execute a contract's method from another account, use connect
+    // execute a contract's method from another account, use connect
     let vote = await ct.connect(addr1).getCurrentVotes(alice.address)
     console.log(vote.toString())
     assert.equal(vote, 0)
