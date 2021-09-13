3acaaa545ac95af36d634fb3a07a79183dd09f2c
yeosiowvic <victoryeo1974@gmail.com>
2021-09-13 15:38:21 +0800

    deploy contract in test cases add cake token test case add resolveJsonModule to tsconfig.json

---
+++ src/contracts/test/CakeToken.spec.ts
@@ -0,0 +1,35 @@
+import { ethers } from "hardhat"
+import { Signer, Contract } from "ethers"
+import { MockProvider, deployContract } from 'ethereum-waffle'
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+import { assert } from "chai";
+
+import CakeToken from '../../../artifacts/src/contracts/pool/CakeToken.sol/CakeToken.json'
+
+describe("CakeToken", function () {
+  let accounts: Signer[]
+  const provider = new MockProvider()
+  const [wallet, other] = provider.getWallets()
+  let token: Contract
+  let alice: SignerWithAddress
+  let minter: SignerWithAddress
+
+  beforeEach(async function () {
+    [alice, minter] = await ethers.getSigners()
+    token = await deployContract(wallet, CakeToken, [])
+  })
+
+  it('test', async () => {
+    //to be added
+  })
+
+  it('mint', async () => {
+    console.log(alice.address)
+    let bal = await token.balanceOf(alice.address)
+    console.log(bal.toString())
+    console.log(token)
+    await token.mint(alice.address, 1000);
+    assert.equal((await token.balanceOf(alice.address)).toString(), '1000');
+  })
+
+})
--- src/contracts/test/SousChef.spec.ts
+++ src/contracts/test/SousChef.spec.ts
@@ -1,18 +1,27 @@
-import { ethers } from "hardhat";
-import { Signer } from "ethers";
-import { MockProvider } from 'ethereum-waffle'
+import { ethers } from "hardhat"
+import { Signer } from "ethers"
+import { MockProvider, deployContract } from 'ethereum-waffle'
+import { Contract } from 'ethers'
+
+import CakeToken from '../../../artifacts/src/contracts/pool/CakeToken.sol/CakeToken.json'
+import SousChef from '../../../artifacts/src/contracts/pool/SousChef.sol/SousChef.json'
+import SyrupBar from '../../../artifacts/src/contracts/pool/SyrupBar.sol/SyrupBar.json'
 
 describe("SousChef", function () {
-  let accounts: Signer[];
+  let accounts: Signer[]
   const provider = new MockProvider()
   const [wallet, other] = provider.getWallets()
+  let ct, sb, sc: Contract
 
   beforeEach(async function () {
     accounts = await ethers.getSigners();
-  });
+    ct = await deployContract(wallet, CakeToken, [])
+    sb = await deployContract(wallet, SyrupBar, [ct.address])
+    sc = await deployContract(wallet, SousChef, [sb.address, 40, 300, 400])
+  })
 
   it('test', async () => {
     //to be added
   })
 
-});
+})
