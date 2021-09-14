aeb50e8c34368f5e28efb928e7437d763d84ede3
yeosiowvic <victoryeo1974@gmail.com>
2021-09-14 11:28:09 +0800

    add mock BEP20 contract

---
+++ src/contracts/pool/libs/MockBEP20.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.0;
+
+import "../../lib/token/BEP20.sol";
+
+contract MockBEP20 is BEP20 {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 supply
+    ) BEP20(name, symbol) {
+        _mint(msg.sender, supply);
+
+    }
+}
