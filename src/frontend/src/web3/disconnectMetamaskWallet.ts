c83cecbe25670da3d8a0488f199cb83957f22ab7
yeosiowvic <victoryeo1974@gmail.com>
2021-09-18 19:58:38 +0800

    add disconnect wallet method

---
+++ src/frontend/src/web3/disconnectMetamaskWallet.ts
@@ -0,0 +1,23 @@
+const disconnectMetamaskWallet = async () => {
+  // This method is run when the user clicks the Connect. It connects the
+  // dapp to the user's wallet, and initializes it.
+
+  // To connect to the user's wallet, we have to run this method.
+  // It returns a promise that will resolve to the user's address.
+  try {
+    const [selectedAddress] = await window.ethereum
+      .request({ method: 'wallet_requestPermissions' })
+
+    console.log(selectedAddress)
+    return selectedAddress
+  } catch (error: any) {
+    if (error.code === 4001) {
+      // EIP-1193 userRejectedRequest error
+      console.log('Please connect to MetaMask.');
+    } else {
+      console.error(error);
+    }
+  }
+}
+
+export default disconnectMetamaskWallet
