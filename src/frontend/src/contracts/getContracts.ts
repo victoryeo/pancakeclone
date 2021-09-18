ee6096acef2b241c1efc9c9c7c326bc32529166b
yeosiowvic <victoryeo1974@gmail.com>
2021-09-18 21:02:40 +0800

    add sousChef contract to frontend

--- src/frontend/src/contracts/getContracts.ts
+++ src/frontend/src/contracts/getContracts.ts
@@ -1,7 +1,9 @@
 import { useSelector } from 'react-redux'
 import { RootState } from '../reducers/index';
 import cakeAbi from '../config/abi/cake.json'
+import sousChefAbi from '../config/abi/souschef.json'
 import tokens from '../config/constants/tokens'
+import contracts from '../config/constants/contracts'
 
 export const GetCake = async () => {
   const web3: any = useSelector((state: RootState) => state.reducers.web3)
@@ -24,6 +26,30 @@ export const GetCake = async () => {
   )
   console.log(cakeInst)
   return cakeInst
+}
+
+export const GetSousChef = async () => {
+  const web3: any = useSelector((state: RootState) => state.reducers.web3)
+  console.log(web3)
+  if (web3.eth == undefined)
+  { 
+    console.log('web3 undefined')
+    return null;
+  }
+
+  let accounts: any = await web3.eth.getAccounts()
+  console.log(accounts[0])
+
+  const chainId = process.env.REACT_APP_CHAIN_ID != undefined ? "TEST": "MAIN"
+
+  let address: string = contracts.sousChef[chainId]
+  console.log(address)
+
+  let sousChefInst: any = await new web3.eth.Contract(
+    cakeAbi, address
+  )
+  console.log(sousChefInst)
+  return sousChefInst
 }
 
 export const GetContract = async () => {
